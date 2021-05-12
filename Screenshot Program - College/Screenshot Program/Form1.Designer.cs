5eb7753892ba60a873b959ee6e5d4a702f7b1057
matthewjonascollege <84069120+matthewjonascollege@users.noreply.github.com>
2021-05-12 10:05:58 +0100

    Add files via upload

---
+++ Screenshot Program - College/Screenshot Program/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Screenshot_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtSshotKey = new System.Windows.Forms.TextBox();
+            this.txtSshotLoc = new System.Windows.Forms.TextBox();
+            this.pbLblLoc = new System.Windows.Forms.PictureBox();
+            this.pbLblSshotKey = new System.Windows.Forms.PictureBox();
+            this.pbSshotKeyClear = new System.Windows.Forms.PictureBox();
+            this.pbSshotLocClear = new System.Windows.Forms.PictureBox();
+            this.pbOpenFolder = new System.Windows.Forms.PictureBox();
+            this.pbLocOutlineRight = new System.Windows.Forms.PictureBox();
+            this.pbLocOutlineLeft = new System.Windows.Forms.PictureBox();
+            this.pbLocOutlineBot = new System.Windows.Forms.PictureBox();
+            this.pbLocOutlineTop = new System.Windows.Forms.PictureBox();
+            this.pbKeyOutlineRight = new System.Windows.Forms.PictureBox();
+            this.pbKeyOutlineLeft = new System.Windows.Forms.PictureBox();
+            this.pbKeyOutlineBot = new System.Windows.Forms.PictureBox();
+            this.pbKeyOutlineTop = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLblLoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLblSshotKey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSshotKeyClear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSshotLocClear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpenFolder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineBot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineBot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineTop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSshotKey
+            // 
+            this.txtSshotKey.BackColor = System.Drawing.Color.White;
+            this.txtSshotKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtSshotKey.Location = new System.Drawing.Point(16, 136);
+            this.txtSshotKey.MaxLength = 1;
+            this.txtSshotKey.Name = "txtSshotKey";
+            this.txtSshotKey.ReadOnly = true;
+            this.txtSshotKey.Size = new System.Drawing.Size(50, 23);
+            this.txtSshotKey.TabIndex = 11;
+            this.txtSshotKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSshotKey_KeyDown);
+            // 
+            // txtSshotLoc
+            // 
+            this.txtSshotLoc.BackColor = System.Drawing.Color.White;
+            this.txtSshotLoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtSshotLoc.Location = new System.Drawing.Point(16, 38);
+            this.txtSshotLoc.Name = "txtSshotLoc";
+            this.txtSshotLoc.ReadOnly = true;
+            this.txtSshotLoc.Size = new System.Drawing.Size(180, 23);
+            this.txtSshotLoc.TabIndex = 7;
+            this.txtSshotLoc.Click += new System.EventHandler(this.txtSshotLoc_Click);
+            // 
+            // pbLblLoc
+            // 
+            this.pbLblLoc.BackColor = System.Drawing.Color.Transparent;
+            this.pbLblLoc.Location = new System.Drawing.Point(16, 16);
+            this.pbLblLoc.Name = "pbLblLoc";
+            this.pbLblLoc.Size = new System.Drawing.Size(161, 12);
+            this.pbLblLoc.TabIndex = 17;
+            this.pbLblLoc.TabStop = false;
+            // 
+            // pbLblSshotKey
+            // 
+            this.pbLblSshotKey.BackColor = System.Drawing.Color.Transparent;
+            this.pbLblSshotKey.Location = new System.Drawing.Point(16, 114);
+            this.pbLblSshotKey.Name = "pbLblSshotKey";
+            this.pbLblSshotKey.Size = new System.Drawing.Size(99, 15);
+            this.pbLblSshotKey.TabIndex = 18;
+            this.pbLblSshotKey.TabStop = false;
+            // 
+            // pbSshotKeyClear
+            // 
+            this.pbSshotKeyClear.BackColor = System.Drawing.Color.Transparent;
+            this.pbSshotKeyClear.Location = new System.Drawing.Point(76, 136);
+            this.pbSshotKeyClear.Name = "pbSshotKeyClear";
+            this.pbSshotKeyClear.Size = new System.Drawing.Size(55, 23);
+            this.pbSshotKeyClear.TabIndex = 19;
+            this.pbSshotKeyClear.TabStop = false;
+            this.pbSshotKeyClear.Click += new System.EventHandler(this.pbSshotKeyClear_Click);
+            this.pbSshotKeyClear.MouseEnter += new System.EventHandler(this.pbSshotKeyClear_MouseEnter);
+            this.pbSshotKeyClear.MouseLeave += new System.EventHandler(this.pbSshotKeyClear_MouseLeave);
+            // 
+            // pbSshotLocClear
+            // 
+            this.pbSshotLocClear.BackColor = System.Drawing.Color.Transparent;
+            this.pbSshotLocClear.Location = new System.Drawing.Point(141, 71);
+            this.pbSshotLocClear.Name = "pbSshotLocClear";
+            this.pbSshotLocClear.Size = new System.Drawing.Size(55, 23);
+            this.pbSshotLocClear.TabIndex = 20;
+            this.pbSshotLocClear.TabStop = false;
+            this.pbSshotLocClear.Click += new System.EventHandler(this.pbSshotLocClear_Click);
+            this.pbSshotLocClear.MouseEnter += new System.EventHandler(this.pbSshotLocClear_MouseEnter);
+            this.pbSshotLocClear.MouseLeave += new System.EventHandler(this.pbSshotLocClear_MouseLeave);
+            // 
+            // pbOpenFolder
+            // 
+            this.pbOpenFolder.BackColor = System.Drawing.Color.Transparent;
+            this.pbOpenFolder.Location = new System.Drawing.Point(16, 71);
+            this.pbOpenFolder.Name = "pbOpenFolder";
+            this.pbOpenFolder.Size = new System.Drawing.Size(98, 23);
+            this.pbOpenFolder.TabIndex = 21;
+            this.pbOpenFolder.TabStop = false;
+            this.pbOpenFolder.Click += new System.EventHandler(this.pbOpenFolder_Click);
+            this.pbOpenFolder.MouseEnter += new System.EventHandler(this.pbOpenFolder_MouseEnter);
+            this.pbOpenFolder.MouseLeave += new System.EventHandler(this.pbOpenFolder_MouseLeave);
+            // 
+            // pbLocOutlineRight
+            // 
+            this.pbLocOutlineRight.BackColor = System.Drawing.Color.Black;
+            this.pbLocOutlineRight.Location = new System.Drawing.Point(195, 39);
+            this.pbLocOutlineRight.Name = "pbLocOutlineRight";
+            this.pbLocOutlineRight.Size = new System.Drawing.Size(1, 21);
+            this.pbLocOutlineRight.TabIndex = 29;
+            this.pbLocOutlineRight.TabStop = false;
+            // 
+            // pbLocOutlineLeft
+            // 
+            this.pbLocOutlineLeft.BackColor = System.Drawing.Color.Black;
+            this.pbLocOutlineLeft.Location = new System.Drawing.Point(16, 39);
+            this.pbLocOutlineLeft.Name = "pbLocOutlineLeft";
+            this.pbLocOutlineLeft.Size = new System.Drawing.Size(1, 21);
+            this.pbLocOutlineLeft.TabIndex = 28;
+            this.pbLocOutlineLeft.TabStop = false;
+            // 
+            // pbLocOutlineBot
+            // 
+            this.pbLocOutlineBot.BackColor = System.Drawing.Color.Black;
+            this.pbLocOutlineBot.Location = new System.Drawing.Point(16, 60);
+            this.pbLocOutlineBot.Name = "pbLocOutlineBot";
+            this.pbLocOutlineBot.Size = new System.Drawing.Size(180, 1);
+            this.pbLocOutlineBot.TabIndex = 27;
+            this.pbLocOutlineBot.TabStop = false;
+            // 
+            // pbLocOutlineTop
+            // 
+            this.pbLocOutlineTop.BackColor = System.Drawing.Color.Black;
+            this.pbLocOutlineTop.Location = new System.Drawing.Point(16, 38);
+            this.pbLocOutlineTop.Name = "pbLocOutlineTop";
+            this.pbLocOutlineTop.Size = new System.Drawing.Size(180, 1);
+            this.pbLocOutlineTop.TabIndex = 26;
+            this.pbLocOutlineTop.TabStop = false;
+            // 
+            // pbKeyOutlineRight
+            // 
+            this.pbKeyOutlineRight.BackColor = System.Drawing.Color.Black;
+            this.pbKeyOutlineRight.Location = new System.Drawing.Point(65, 137);
+            this.pbKeyOutlineRight.Name = "pbKeyOutlineRight";
+            this.pbKeyOutlineRight.Size = new System.Drawing.Size(1, 21);
+            this.pbKeyOutlineRight.TabIndex = 33;
+            this.pbKeyOutlineRight.TabStop = false;
+            // 
+            // pbKeyOutlineLeft
+            // 
+            this.pbKeyOutlineLeft.BackColor = System.Drawing.Color.Black;
+            this.pbKeyOutlineLeft.Location = new System.Drawing.Point(16, 137);
+            this.pbKeyOutlineLeft.Name = "pbKeyOutlineLeft";
+            this.pbKeyOutlineLeft.Size = new System.Drawing.Size(1, 21);
+            this.pbKeyOutlineLeft.TabIndex = 32;
+            this.pbKeyOutlineLeft.TabStop = false;
+            // 
+            // pbKeyOutlineBot
+            // 
+            this.pbKeyOutlineBot.BackColor = System.Drawing.Color.Black;
+            this.pbKeyOutlineBot.Location = new System.Drawing.Point(16, 158);
+            this.pbKeyOutlineBot.Name = "pbKeyOutlineBot";
+            this.pbKeyOutlineBot.Size = new System.Drawing.Size(50, 1);
+            this.pbKeyOutlineBot.TabIndex = 31;
+            this.pbKeyOutlineBot.TabStop = false;
+            // 
+            // pbKeyOutlineTop
+            // 
+            this.pbKeyOutlineTop.BackColor = System.Drawing.Color.Black;
+            this.pbKeyOutlineTop.Location = new System.Drawing.Point(16, 136);
+            this.pbKeyOutlineTop.Name = "pbKeyOutlineTop";
+            this.pbKeyOutlineTop.Size = new System.Drawing.Size(50, 1);
+            this.pbKeyOutlineTop.TabIndex = 30;
+            this.pbKeyOutlineTop.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(534, 288);
+            this.Controls.Add(this.pbKeyOutlineRight);
+            this.Controls.Add(this.pbKeyOutlineLeft);
+            this.Controls.Add(this.pbKeyOutlineBot);
+            this.Controls.Add(this.pbKeyOutlineTop);
+            this.Controls.Add(this.pbLocOutlineRight);
+            this.Controls.Add(this.pbLocOutlineLeft);
+            this.Controls.Add(this.pbLocOutlineBot);
+            this.Controls.Add(this.pbLocOutlineTop);
+            this.Controls.Add(this.pbOpenFolder);
+            this.Controls.Add(this.pbSshotLocClear);
+            this.Controls.Add(this.pbSshotKeyClear);
+            this.Controls.Add(this.pbLblSshotKey);
+            this.Controls.Add(this.pbLblLoc);
+            this.Controls.Add(this.txtSshotKey);
+            this.Controls.Add(this.txtSshotLoc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "10/10 Screenshot Programerino";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLblLoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLblSshotKey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSshotKeyClear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSshotLocClear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpenFolder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineBot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLocOutlineTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineBot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKeyOutlineTop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSshotKey;
+        private System.Windows.Forms.TextBox txtSshotLoc;
+        private System.Windows.Forms.PictureBox pbLblLoc;
+        private System.Windows.Forms.PictureBox pbLblSshotKey;
+        private System.Windows.Forms.PictureBox pbSshotKeyClear;
+        private System.Windows.Forms.PictureBox pbSshotLocClear;
+        private System.Windows.Forms.PictureBox pbOpenFolder;
+        private System.Windows.Forms.PictureBox pbLocOutlineRight;
+        private System.Windows.Forms.PictureBox pbLocOutlineLeft;
+        private System.Windows.Forms.PictureBox pbLocOutlineBot;
+        private System.Windows.Forms.PictureBox pbLocOutlineTop;
+        private System.Windows.Forms.PictureBox pbKeyOutlineRight;
+        private System.Windows.Forms.PictureBox pbKeyOutlineLeft;
+        private System.Windows.Forms.PictureBox pbKeyOutlineBot;
+        private System.Windows.Forms.PictureBox pbKeyOutlineTop;
+    }
+}
+
---
+++ Screenshot Program - College/Screenshot Program/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Screenshot_Program
+{
+    public partial class Form1 : Form
+    {
+        // Creating a custom key and a bool to check if a key is pressed.
+        Keys sshotKey;
+        bool isKeyPressed = false;
+
+        // Create new global keyboard hook object.
+        GlobalKeyboardHook gHook = new GlobalKeyboardHook();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set key handling events for the global hook and add the keys.
+            gHook.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            gHook.KeyUp += new KeyEventHandler(Form1_KeyUp);
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+            {
+                gHook.HookedKeys.Add(key);
+            }
+
+            // Allows the form to handle key presses before controls, idk if it's required but w/e.
+            this.KeyPreview = true;
+            
+            // Set the background image, images for labels, and images for pseudo-buttons (pictureboxes which act as buttons).
+            this.BackgroundImage = Images.Background;
+            pbLblLoc.Image = Images.SaveLocation;
+            pbLblSshotKey.Image = Images.ScreenshotKey;
+            pbSshotLocClear.Image = Images.Clear;
+            pbOpenFolder.Image = Images.OpenFolder;
+            pbSshotKeyClear.Image = Images.Clear;
+
+            // Setting the active control to a label for the cleanse UI of dreams (this appears a lot in this program. :)
+            ActiveControl = pbLblLoc;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Sets the values inside the textboxes to the saved default values when the form is opened and load the saved the location.
+            txtSshotLoc.Text = Properties.Settings.Default.ScreenshotLocation;
+            txtSshotKey.Text = Properties.Settings.Default.ScreenshotKey;
+            this.Location = Properties.Settings.Default.Location;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Saves the values inside the textboxes and the location to the new default values when the form is closed.
+            Properties.Settings.Default.ScreenshotLocation = txtSshotLoc.Text;
+            Properties.Settings.Default.ScreenshotKey = txtSshotKey.Text;
+            Properties.Settings.Default.Location = this.Location;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Changes the key value to the value in the textbox.
+            Enum.TryParse(txtSshotKey.Text, out sshotKey);
+
+            // Checks if the pressed key = the custom key and if the screenshot key textbox isn't highlighted
+            // (because if you change the screenshot key to the same key then it will take a screenshot which shouldn't happen).
+            if (e.KeyCode == sshotKey && txtSshotKey.Focused == false)
+            {
+                // If the key isn't already pressed do this.
+                if (!isKeyPressed)
+                {
+                    // Call the Screenshot method.
+                    Screenshot();
+
+                    // Set isPressed to true.
+                    isKeyPressed = true;
+
+                    // Surpresses the key so that it doesn't interfere with other applications (i.e. if the key was F5 then it WON'T refresh a webpage).
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Again, changes the key value to the value in the textbox.
+            Enum.TryParse(txtSshotKey.Text, out sshotKey);
+
+            // Checks if the un-pressed key = the custom key.
+            if (e.KeyCode == sshotKey)
+            {
+                // If it's un-pressed, set the isPressed bool to false.
+                isKeyPressed = false;
+            }
+        }
+
+        private void txtSshotLoc_Click(object sender, EventArgs e)
+        {
+            // Opens a new FBD when the textbox is clicked.
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Sets the textbox to the selected directory.
+                txtSshotLoc.Text = dialog.SelectedPath;
+            }
+
+            ActiveControl = pbLblLoc;
+        }
+
+        private void pbSshotLocClear_Click(object sender, EventArgs e)
+        {
+            // Resets the directory textbox.
+            txtSshotLoc.Text = "C:";
+
+            ActiveControl = pbLblLoc;
+        }
+
+        private void pbOpenFolder_Click(object sender, EventArgs e)
+        {
+            // Simply opens the folder where the screenshots are saved.
+            try
+            {
+                string path = txtSshotLoc.Text;
+                System.Diagnostics.Process.Start(path);
+            }
+            catch
+            {
+
+            }
+
+            ActiveControl = pbLblLoc;
+        }
+
+        private void txtSshotKey_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If the textbox is empty then do this.
+            if (txtSshotKey.Text == "")
+            {
+                // Sets the textbox to the key which was pressed.
+                txtSshotKey.Text += e.KeyCode.ToString();
+            }
+            // If the textbox isn't empty then do this.
+            else
+            {
+                // If there was already a value within the textbox, then it clears it.
+                // Then is sets the textbox to the key which was pressed.
+                txtSshotKey.Clear();
+                txtSshotKey.Text += e.KeyCode.ToString();
+            }
+
+            // Surpresses the key.
+            e.Handled = true;
+
+            ActiveControl = pbLblLoc;
+        }
+
+        private void pbSshotKeyClear_Click(object sender, EventArgs e)
+        {
+            // Clears the key textbox.
+            txtSshotKey.Clear();
+
+            ActiveControl = pbLblLoc;
+        }
+
+        #region MouseEnter & MouseLeave Events.
+
+        // When the mouse enters the picturebox, it changes the image of the picturebox 
+        // to a different one to show that it is selected and vice versa.
+        private void pbSshotLocClear_MouseEnter(object sender, EventArgs e)
+        {
+            pbSshotLocClear.Image = Images.ClearSelected;
+        }
+
+        private void pbSshotLocClear_MouseLeave(object sender, EventArgs e)
+        {
+            pbSshotLocClear.Image = Images.Clear;
+        }
+
+        private void pbOpenFolder_MouseEnter(object sender, EventArgs e)
+        {
+            pbOpenFolder.Image = Images.OpenFolderSelected;
+        }
+
+        private void pbOpenFolder_MouseLeave(object sender, EventArgs e)
+        {
+            pbOpenFolder.Image = Images.OpenFolder;
+        }
+
+        private void pbSshotKeyClear_MouseEnter(object sender, EventArgs e)
+        {
+            pbSshotKeyClear.Image = Images.ClearSelected;
+        }
+
+        private void pbSshotKeyClear_MouseLeave(object sender, EventArgs e)
+        {
+            pbSshotKeyClear.Image = Images.Clear;
+        }
+        #endregion
+
+        void Screenshot()
+        {
+            // Creates a new Rectangle with the bounds of the monitor.
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            // Creates a new Bitmap using the size of the Rectangle.
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                // Creates new graphics using the Bitmap.
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    // Sets the InterpolationMode to high (idk if this actually changes anything lol).
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                    // Copies the graphics from the screen using the size of the Rectangle.
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                // Creates a savePath string which takes the directory and adds on a unique part to the end based on DateTime.
+                // (This means there aren't any overwrite issues.)
+                string path = txtSshotLoc.Text + "\\Screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".bmp";
+                // Saves the Bitmap to the savePath as a .bmp.
+                bitmap.Save(path, System.Drawing.Imaging.ImageFormat.Bmp);
+            }
+        }
+    }
+}
